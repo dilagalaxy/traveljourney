0662c7a02005c9e23fc6b0ff97d07823762fc75d
Siti Nuradila <startbeautiful@gmail.com>
2017-04-13 17:03:02 +0800

    add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  
+
+
   resources :posts do
-   resources:comments
+  	resources :comments
   end
 
-  root "posts#index" 
+  root "posts#index"
+
+  get '/about', to: 'pages#about'
+
 end
